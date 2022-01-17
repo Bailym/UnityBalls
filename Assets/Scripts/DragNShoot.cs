6cec31506107f7a8c0279145af26067c53240cf3
Bailym <bm39@hotmail.co.uk>
2022-01-17 19:14:30 +0000

    add basic shooting

---
+++ Assets/Scripts/DragNShoot.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]    //runs before the class is initialised. Requires LR component
+public class DragNShoot : MonoBehaviour
+{
+    public float powerMultiplier = 10f;
+    public Rigidbody2D rb;
+    public LineRenderer lr;
+
+    public Vector2 minPower;    //The min power
+    public Vector2 maxPower;    //max power. Stops the user getting more power on a bigger screen.
+
+    Camera cam;
+    Vector2 force;  //calculated force
+    Vector3 endPoint;   //end of drag
+    Vector3 objPoint;   //the point of this object. Used as an origin for the linerender
+    Vector3 currentPoint;   //the current mouse point
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();  //reference for the line renderer.
+    }
+
+    private void Start()
+    {
+        cam = Camera.main;  //set to main camera
+        objPoint = GetComponent<Transform>().position;  //get the position of this obj
+    }
+
+    private void Update()
+    {
+ 
+        //if the left click is currently down.
+        if (Input.GetMouseButton(0))
+        {
+            currentPoint = cam.ScreenToWorldPoint(Input.mousePosition); //get the current mouse pos
+            currentPoint.z = 15;    //move to top layer
+            lr.positionCount = 2;   //shows the line renderer
+
+            Vector3[] points = new Vector3[2];  //points in the line renderer
+            points[1] = objPoint;   //start from the object
+            points[0] = currentPoint;   //draw to the mouse
+
+            lr.SetPositions(points);    //set the positions
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition); //set the end point to the mouse position - mouse position used pixels by default so it needs to be converted to world co ordinates
+            endPoint.z = 15;  //move start point to top of scene layers.
+
+
+            force = new Vector2(Mathf.Clamp(objPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(objPoint.y - endPoint.y, minPower.y, maxPower.y));    //calculate the force
+
+            rb.AddForce(force * powerMultiplier, ForceMode2D.Impulse);    //add the force to the rigidbody
+
+        }
+    }
+
+}
